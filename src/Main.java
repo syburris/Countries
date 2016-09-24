56dc3b9c3fc1118b4e475e6090b5fb0575cbad2f
Steven Burris <stevenburris.jr@gmail.com>
2016-09-24 02:35:50 -0400

    I can't figure out how to print out the array list

--- src/Main.java
+++ src/Main.java
@@ -12,17 +12,30 @@ public class Main {
 
     static final String COUNTRIES = "countries.txt";
     public static HashMap<String, ArrayList<Country>> country;
+    public static ArrayList<Country> countries = new ArrayList<>();
 
     public static void main(String[] args) {
         read(COUNTRIES);
         System.out.println("enter a letter");
         Scanner scanner = new Scanner(System.in);
+        Character countryAbbreviation = 'a';
 
+        //loop over posts and print the ones with correct replyID
+        while (true) {
+            Character abbrev = scanner.nextLine().charAt(0);
+            for ( Country country : countries) {
+                if (country.countryAbbreviation.substring() == countryAbbreviation) {
+                    System.out.printf("%s",country.countryName);
+                }
+
+            }
+            System.out.println("Please enter letter");
+            countryAbbreviation = scanner.nextLine();
+        }
 
     }
 
     public static ArrayList<Country> read(String countryFile) {
-        ArrayList<Country> countries = new ArrayList<>();
 
         // read the file and place them into hash map
         File file = new File(COUNTRIES);
