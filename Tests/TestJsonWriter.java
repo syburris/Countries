1b7eb94a8c8f4b6b3bcd9d7f30c07af8b4ae4eea
Steven Burris <stevenburris.jr@gmail.com>
2016-09-26 12:44:12 -0400

    All Tests work

--- Tests/TestJsonWriter.java
+++ Tests/TestJsonWriter.java
@@ -1,20 +1,34 @@
+import jodd.json.JsonSerializer;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by stevenburris on 9/26/16.
  */
 public class TestJsonWriter {
     @Test
-    public void testJsonWriter(){
+    public void testJsonWriter() throws IOException {
         //Given
-
-
+        ArrayList<Country> countries= Main.read("countries.txt");
 
         //When
-
-
+        File countryJson = new File("A" + "_countries.json");
+        JsonSerializer serializer = new JsonSerializer();
+        CountryWrapper wrapper = new CountryWrapper();
+        wrapper.country = countries;
+        String json = serializer.deep(true).serialize(wrapper);
+        FileWriter writeJson = new FileWriter(countryJson);
+        writeJson.write(json);
+        writeJson.close();
 
         //Then
+        assertTrue(countryJson.exists());
 
 
 
--- Tests/TestTxtWriter.java
+++ Tests/TestTxtWriter.java
@@ -1,18 +1,32 @@
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by stevenburris on 9/25/16.
  */
 public class TestTxtWriter {
 
     @Test
-    public void testTxtWriter(){
-        //Given
+    public void testTxtWriter() throws IOException {
 
-
+        ArrayList<Country> countries= Main.read("countries.txt");
         //When
+        File countryFile = new File("A" + "_countries.txt");
+        FileWriter fileWriter = new FileWriter(countryFile);
+        for (Country country : countries) {
+            fileWriter.append(country.toString() + "\n");
+        }
+        fileWriter.close();
 
         //Then
+
+        assertTrue(countryFile.exists());
 
 
     }
